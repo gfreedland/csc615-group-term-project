4a61e1d6eef14cb240a49775ba5eca88936882f8
g.freedland <gfreedland@mail.sfsu.edu>
2020-12-17 11:40:25 -0800

    fixes in struture

--- main.c
+++ main.c
@@ -17,39 +17,31 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 
-//TEMPORARY DEFITIONS: Change as pins are chosen
-// //Line Sensor Voltage Control and Digital Output
-// #define LINE_SENSOR_L_VCC 0;
-#define LINE_SENSOR_L_DO 7 // On board IR1
-// #define LINE_SENSOR_R_VCC 0;
+// Line Sensor Voltage Control and Digital Output
+#define LINE_SENSOR_L_DO 7  // On board IR1
 #define LINE_SENSOR_R_DO 12 // On board IR2
-// #define LINE_SENSOR_C_VCC 0;
 #define LINE_SENSOR_C_DO 27 // Physical pin 36
-//TODO define remaining sensors
-// #define ECHO_VCC 0 // Don't need this as auto powered
 #define ECHO_TRIG 21
 #define ECHO_ECHO 22
-//update existing defitions with corresponding pin numbers
-
 //front right
 #define MOTOR_1_ENABLE 0    // 11 physical pin for motor 1
 #define MOTOR_1_CONTROL_1 3 // 15 physical pin for motor 1
 #define MOTOR_1_CONTROL_2 2 // 13 physical pin for motor 1
 
 //front left
-#define MOTOR_2_ENABLE 6    // 11 physical pin for motor 2
-#define MOTOR_2_CONTROL_1 4 // 13 physical pin for motor 2
-#define MOTOR_2_CONTROL_2 5 // 15 physical pin for motor 2
+#define MOTOR_2_ENABLE 6    // 22 physical pin for motor 2
+#define MOTOR_2_CONTROL_1 4 // 16 physical pin for motor 2
+#define MOTOR_2_CONTROL_2 5 // 18 physical pin for motor 2
 
 //back left
-#define MOTOR_3_ENABLE 12    // 11 physical pin for motor 3
-#define MOTOR_3_CONTROL_1 14 // 15 physical pin for motor 3
-#define MOTOR_3_CONTROL_2 13 // 13 physical pin for motor 3
+#define MOTOR_3_ENABLE 12    // 19 physical pin for motor 3
+#define MOTOR_3_CONTROL_1 14 // 21 physical pin for motor 3
+#define MOTOR_3_CONTROL_2 13 // 23 physical pin for motor 3
 
 //back right
-#define MOTOR_4_ENABLE 26    // 11 physical pin for motor 4
-#define MOTOR_4_CONTROL_1 10 // 13 physical pin for motor 4
-#define MOTOR_4_CONTROL_2 11 // 15 physical pin for motor 4
+#define MOTOR_4_ENABLE 26    // 32 physical pin for motor 4
+#define MOTOR_4_CONTROL_1 10 // 25 physical pin for motor 4
+#define MOTOR_4_CONTROL_2 11 // 27 physical pin for motor 4
 
 const int MILLISEC = 1000;
 const int OBSTACLE_WAIT_TIME = 5;
@@ -71,10 +63,12 @@ typedef struct args
   int motor3_c2;
   int motor4_c1;
   int motor4_c2;
-
   //add a past adjustments stack if necessary
 } args;
 
+/**************************************************
+* Initialization and clear functions.
+***************************************************/
 void initilalizePins()
 {
   // Inits motor 1
@@ -159,6 +153,9 @@ void clearPins()
   softPwmWrite(MOTOR_4_CONTROL_2, 0);
 }
 
+/**************************************************
+* Car movement functions.
+***************************************************/
 void stopCar()
 {
   softPwmWrite(MOTOR_1_CONTROL_1, 0);
@@ -218,84 +215,31 @@ void spinCarLeft()
   softPwmWrite(MOTOR_4_CONTROL_1, 50);
 }
 
-int main(void)
+/**************************************************
+* Threaded functions and methods that they use.
+***************************************************/
+void updateDistance(unsigned int *start, double *total, double *cm)
 {
-  //printf("Welcome to the Raspberry Pi Self Driving \n");
-  wiringPiSetup();
-  initilalizePins();
-
-  pthread_t ir;
-  pthread_t line;
-  args arguments;
-  arguments.runFlag = 1;
-  arguments.obstacleDetected = 0;
-
-  arguments.motor1_c1 = OPTIMAL_SPEED;
-  arguments.motor1_c2 = OPTIMAL_SPEED;
-  arguments.motor2_c1 = OPTIMAL_SPEED;
-  arguments.motor2_c2 = OPTIMAL_SPEED;
-  arguments.motor3_c1 = OPTIMAL_SPEED;
-  arguments.motor3_c2 = OPTIMAL_SPEED;
-  arguments.motor4_c1 = OPTIMAL_SPEED;
-  arguments.motor4_c2 = OPTIMAL_SPEED;
-
-  bool condition = true;
-  while (condition)
+  digitalWrite(ECHO_TRIG, 0);
+  delayMicroseconds(10);
+  digitalWrite(ECHO_TRIG, 1);
+  delayMicroseconds(10);
+  digitalWrite(ECHO_TRIG, 0);
+  *start = micros();
+  while (digitalRead(ECHO_ECHO) == 0)
   {
-    int input;
-    printf("Enter 1 to start all motors forward, Enter 2 to stop all motors, and 3 to exit the program");
-    printf("Enter 4 to go backwards, Enter 5 to turn right, Enter 6 to turn left, Enter 7 to test Program");
-
-    scanf("%d", &input);
-    switch (input)
-    {
-    case 1:
-      printf("Move all motors forward\n");
-      initilalizePins();
-      moveCarForward();
-      break;
-    case 2:
-      printf("Stop all motors\n");
-      stopCar();
-      break;
-    case 3:
-      printf("Exitting program\n");
-      clearPins();
-      condition = false;
-      break;
-    case 4:
-      printf("Move all motors backwards\n");
-      moveCarBackward();
-      break;
-    case 5:
-      printf("Spin car Right In Place\n");
-      spinCarRight();
-      break;
-    case 6:
-      printf("Spin car Left In Place\n");
-      spinCarLeft();
-      break;
-    case 7:
-      printf("Spin car Left In Place\n");
-      moveCarForward();
-      //Run program until threads finish
-      pthread_create(&ir, NULL, echoSensor, &arguments);
-      pthread_create(&line, NULL, lineSensor, &arguments);
-      /*Join/Wait for threads to finish
-      pthread_join(ir, NULL);
-      pthread_join(line, NULL);*/
-      break;
-    default:
-      printf("not a valid input");
-      break;
-    }
+    *start = micros();
   }
-  printf("Program Ended");
-  return 0;
+  while (digitalRead(ECHO_ECHO) == 1)
+  {
+    *total = micros();
+  }
+  *total = total - start;
+  *cm = (*total / 2.0) * .0340;
 }
 
-//temp thread, taken from assignment 5
-//checks for obstacles
+// Temp thread, taken from assignment 5
+// Checks for obstacles
 void *echoSensor(void *value)
 {
   args *arguments = (struct args *)value;
@@ -325,28 +269,8 @@ void *echoSensor(void *value)
   }
 }
 
-void updateDistance(unsigned int *start, double *total, double *cm)
-{
-  digitalWrite(ECHO_TRIG, 0);
-  delayMicroseconds(10);
-  digitalWrite(ECHO_TRIG, 1);
-  delayMicroseconds(10);
-  digitalWrite(ECHO_TRIG, 0);
-  *start = micros();
-  while (digitalRead(ECHO_ECHO) == 0)
-  {
-    *start = micros();
-  }
-  while (digitalRead(ECHO_ECHO) == 1)
-  {
-    *total = micros();
-  }
-  *total = total - start;
-  *cm = (*total / 2.0) * .0340;
-}
-
-//temp thread, taken from assignment 5
-//checks for line, if not avoiding obstacle this adjusts heading
+// Temp thread, taken from assignment 5
+// Checks for line, if not avoiding obstacle this adjusts heading
 void *lineSensor(void *value)
 {
   args *arguments = (struct args *)value;
@@ -448,4 +372,83 @@ void *lineSensor(void *value)
       }
     }
   }
+}
+
+/**************************************************
+* Main driver function for the program
+***************************************************/
+int main(void)
+{
+  //printf("Welcome to the Raspberry Pi Self Driving \n");
+  wiringPiSetup();
+  initilalizePins();
+
+  pthread_t ir;
+  pthread_t line;
+  args arguments;
+  arguments.runFlag = 1;
+  arguments.obstacleDetected = 0;
+
+  arguments.motor1_c1 = OPTIMAL_SPEED;
+  arguments.motor1_c2 = OPTIMAL_SPEED;
+  arguments.motor2_c1 = OPTIMAL_SPEED;
+  arguments.motor2_c2 = OPTIMAL_SPEED;
+  arguments.motor3_c1 = OPTIMAL_SPEED;
+  arguments.motor3_c2 = OPTIMAL_SPEED;
+  arguments.motor4_c1 = OPTIMAL_SPEED;
+  arguments.motor4_c2 = OPTIMAL_SPEED;
+
+  bool condition = true;
+  while (condition)
+  {
+    int input;
+    printf("Enter 1 to start all motors forward, Enter 2 to stop all motors, and 3 to exit the program");
+    printf("Enter 4 to go backwards, Enter 5 to turn right, Enter 6 to turn left, Enter 7 to test Program");
+
+    scanf("%d", &input);
+    switch (input)
+    {
+    case 1:
+      printf("Move all motors forward\n");
+      initilalizePins();
+      moveCarForward();
+      break;
+    case 2:
+      printf("Stop all motors\n");
+      stopCar();
+      break;
+    case 3:
+      printf("Exitting program\n");
+      clearPins();
+      condition = false;
+      break;
+    case 4:
+      printf("Move all motors backwards\n");
+      moveCarBackward();
+      break;
+    case 5:
+      printf("Spin car Right In Place\n");
+      spinCarRight();
+      break;
+    case 6:
+      printf("Spin car Left In Place\n");
+      spinCarLeft();
+      break;
+    case 7:
+      printf("Spin car Left In Place\n");
+      moveCarForward();
+      //Run program until threads finish
+      pthread_create(&ir, NULL, echoSensor, &arguments);
+      pthread_create(&line, NULL, lineSensor, &arguments);
+      /*Join/Wait for threads to finish
+      pthread_join(ir, NULL);
+      pthread_join(line, NULL);*/
+      break;
+    default:
+      printf("not a valid input");
+      break;
+    }
+  }
+  printf("Program Ended");
+  return 0;
 }
