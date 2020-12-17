66ff00335e74ce8064e56d77d8f7045aee4fbf75
g.freedland <gfreedland@mail.sfsu.edu>
2020-12-17 14:48:39 -0800

    fix

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ const int MILLISEC = 1000;
 const int OBSTACLE_WAIT_TIME = 5;
 const int OBSTACLE_DISTANCE = 25;
 const int MAX_RUN_TIME = 90;
-const int OPTIMAL_SPEED = 10;
+const int OPTIMAL_SPEED = 14;
 const int MIN_SPEED = 5;
 const int ADJUST = 8;
 
