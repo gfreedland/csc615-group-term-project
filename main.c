0b6e41ffd70e4f1bb07f3ae88de386a39b02f8db
g.freedland <gfreedland@mail.sfsu.edu>
2020-12-17 12:12:47 -0800

    changed optimal speed

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ const int MILLISEC = 1000;
 const int OBSTACLE_WAIT_TIME = 5;
 const int OBSTACLE_DISTANCE = 50;
 const int MAX_RUN_TIME = 90;
-const int OPTIMAL_SPEED = 50;
+const int OPTIMAL_SPEED = 25;
 const int MIN_SPEED = 10;
 const int ADJUST = 5;
 
