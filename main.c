22b5eaa285de5a6a9821f5e516ba8ec3423f5f52
g.freedland <gfreedland@mail.sfsu.edu>
2020-12-17 14:52:49 -0800

    fix

--- main.c
+++ main.c
@@ -47,9 +47,9 @@ const int MILLISEC = 1000;
 const int OBSTACLE_WAIT_TIME = 5;
 const int OBSTACLE_DISTANCE = 25;
 const int MAX_RUN_TIME = 90;
-const int OPTIMAL_SPEED = 18;
+const int OPTIMAL_SPEED = 15;
 const int MIN_SPEED = 10;
-const int ADJUST = 8;
+const int ADJUST = 5;
 
 typedef struct args
 {
